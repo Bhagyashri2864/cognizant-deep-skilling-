e44bd36bd72cd4a23f59fda71cdcbbefd48e8458
Bhagyashri2864 <tanusalunkhe2864@gmail.com>
2026-06-20 18:43:25 +0530

    Completed Exercise 5 decorator Pattern

---
+++ week 1/design patterns and principles/Exercise5-DecoratorPattern/TestDecorator.java
@@ -0,0 +1,12 @@
+public class TestDecorator {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send();
+    }
+}
